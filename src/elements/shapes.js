d86cde5f28dc589e7a5b0fa3c3cd0efd5feb163b
Elsa Delmas <elsad@datagif.fr>
2021-11-09 11:35:39 +0100

    clean shapes

--- src/elements/shapes.js
+++ src/elements/shapes.js
@@ -1,7 +1,7 @@
 export const shapes = [
   {
     id: 0,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M3.6742e-05 32.1428C17.752 32.1429 32.1429 17.752 32.1429 -1.56112e-06L67.8572 0C67.8572 37.4765 37.4765 67.8571 3.51808e-05 67.8571L3.6742e-05 32.1428Z"/>
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M0.000286503 32.1429C17.7523 32.1429 32.1431 17.752 32.1431 4.68336e-06L67.8574 0C67.8574 37.4765 37.4768 67.8571 0.000291186 67.8571L0.000286503 32.1429Z"/>
     `,
     constraints: {
       top: true,
@@ -14,7 +14,7 @@ export const shapes = [
     id: 1,
     svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M67.8572 -0.000206896C67.8571 17.7518 82.248 32.1426 100 32.1427L100 67.8569C62.5235 67.8569 32.1429 37.4763 32.1429 -0.000210019L67.8572 -0.000206896Z"/>
     <path fill-rule="evenodd" clip-rule="evenodd" d="M9.53675e-07 32.1431L100 32.1431L100 67.8574L9.53674e-07 67.8574L9.53675e-07 32.1431Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M67.8572 99.8571C67.8571 82.1051 82.248 67.7143 100 67.7143L100 32C62.5235 32 32.1429 62.3807 32.1429 99.8571L67.8572 99.8571Z"/>    
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M67.8572 99.9997C67.8571 82.2477 82.248 67.8569 100 67.8569L100 32.1426C62.5235 32.1426 32.1429 62.5233 32.1429 99.9997L67.8572 99.9997Z"/>
     `,
     constraints: {
       top: true,
@@ -84,7 +84,7 @@ export const shapes = [
   },
   {
     id: 7,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M3.6742e-05 67.7141C17.752 67.7141 32.1429 82.1049 32.1429 99.8569L67.8572 99.8569C67.8572 62.3805 37.4765 31.9998 3.51808e-05 31.9998L3.6742e-05 67.7141Z"/>
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M32.1429 100C32.1429 82.248 17.752 67.8571 0 67.8571L0 32.1428C37.4765 32.1428 67.8571 62.5235 67.8571 100L32.1429 100Z"/>
     `,
     constraints: {
       top: false,
@@ -96,8 +96,8 @@ export const shapes = [
   {
     id: 8,
     svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M3.86493e-05 32.1429C17.752 32.1429 32.1429 17.752 32.1429 -1.56112e-06L67.8572 0C67.8572 37.4765 37.4765 67.8571 3.70882e-05 67.8571L3.86493e-05 32.1429Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.1434 100L32.1434 1.56112e-06L67.8577 0L67.8577 100L32.1434 100Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M99.8574 32.1433C82.1054 32.1433 67.7145 17.7525 67.7145 0.000483598L32.0002 0.000488281C32.0002 37.477 62.3809 67.8576 99.8574 67.8576L99.8574 32.1433Z"/>    
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.1429 100L32.1429 1.56112e-06L67.8572 0L67.8572 100L32.1429 100Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M100 32.1428C82.248 32.1428 67.8571 17.752 67.8571 -4.68336e-06L32.1428 0C32.1428 37.4765 62.5235 67.8571 100 67.8571L100 32.1428Z"/>
     `,
     constraints: {
       top: true,
@@ -108,8 +108,8 @@ export const shapes = [
   },
   {
     id: 9,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M67.7143 100L67.7143 1.56112e-06L32 0L32 100L67.7143 100Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M99.8571 32.1429C82.1051 32.1429 67.7143 17.752 67.7143 -1.56112e-06L32 0C32 37.4765 62.3807 67.8571 99.8571 67.8571L99.8571 32.1429Z"/>    
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M67.8569 100L67.8569 1.56112e-06L32.1426 0L32.1426 100L67.8569 100Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M99.9997 32.1429C82.2477 32.1429 67.8569 17.752 67.8569 -1.56112e-06L32.1426 0C32.1426 37.4765 62.5233 67.8571 99.9997 67.8571L99.9997 32.1429Z"/>
     `,
     constraints: {
       top: true,
@@ -132,8 +132,8 @@ export const shapes = [
   },
   {
     id: 11,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M99.8571 32.1429C82.1051 32.1429 67.7143 17.752 67.7143 -1.56112e-06L32 0C32 37.4765 62.3807 67.8571 99.8571 67.8571L99.8571 32.1429Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M67.7143 100C67.7143 82.2482 82.1052 67.8574 99.8572 67.8574V32.1431C62.3807 32.1431 32 62.5237 32 100H67.7143Z"/>    
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M99.9997 67.8571C82.2477 67.8571 67.8569 82.248 67.8569 100L32.1426 100C32.1426 62.5235 62.5233 32.1429 99.9997 32.1429L99.9997 67.8571Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M67.8571 3.38044e-05C67.8571 17.752 82.248 32.1429 100 32.1429L100 67.8572C62.5235 67.8572 32.1429 37.4765 32.1429 3.69267e-05L67.8571 3.38044e-05Z"/>
     `,
     constraints: {
       top: true,
@@ -156,7 +156,7 @@ export const shapes = [
   },
   {
     id: 13,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M99.8571 67.7141C82.1051 67.7141 67.7143 82.1049 67.7143 99.8569L32 99.8569C32 62.3805 62.3807 31.9998 99.8571 31.9998L99.8571 67.7141Z"/>
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M99.9997 67.8572C82.2477 67.8572 67.8569 82.248 67.8569 100L32.1426 100C32.1426 62.5235 62.5233 32.1429 99.9997 32.1429L99.9997 67.8572Z"/>
     `,
     constraints: {
       top: false,
@@ -167,10 +167,9 @@ export const shapes = [
   },
   {
     id: 14,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M32.1429 99.8571C32.1429 82.1051 17.752 67.7143 0 67.7143L0 32C37.4765 32 67.8571 62.3807 67.8571 99.8571L32.1429 99.8571Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M100 67.7138L3.12224e-06 67.7138L0 31.9995L100 31.9995L100 67.7138Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.1428 -0.000209706C32.1428 17.7518 17.752 32.1426 -6.24448e-06 32.1427L0 67.8569C37.4765 67.8569 67.8571 37.4762 67.8571 -0.000215951L32.1428 -0.000209706Z"/>
-    
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M32.1429 100C32.1429 82.2482 17.752 67.8573 0 67.8573V32.1431C37.4765 32.1431 67.8571 62.5237 67.8571 100H32.1429Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M100 67.8569L3.12224e-06 67.8569L0 32.1426L100 32.1426L100 67.8569Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.1428 0.000278575C32.1428 17.7523 17.752 32.1431 -6.24448e-06 32.1431L0 67.8574C37.4765 67.8574 67.8571 37.4767 67.8571 0.00027233L32.1428 0.000278575Z"/>
     `,
     constraints: {
       top: true,
@@ -205,7 +204,7 @@ export const shapes = [
   },
   {
     id: 17,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M99.8571 32.1428C82.1051 32.1429 67.7143 17.752 67.7143 -1.56112e-06L32 0C32 37.4765 62.3807 67.8571 99.8571 67.8571L99.8571 32.1428Z"/>
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M67.8571 3.71253e-05C67.8571 17.752 82.248 32.1429 100 32.1429L100 67.8572C62.5235 67.8572 32.1429 37.4765 32.1429 3.599e-05L67.8571 3.71253e-05Z"/>
     `,
     constraints: {
       top: true,
@@ -216,9 +215,9 @@ export const shapes = [
   },
   {
     id: 18,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M99.8574 67.8572C82.1054 67.8572 67.7145 82.248 67.7145 100L32.0002 100C32.0002 62.5235 62.3809 32.1429 99.8574 32.1429L99.8574 67.8572Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M67.7141 2.5148e-06L67.7141 100L31.9998 100L31.9998 9.53674e-07L67.7141 2.5148e-06Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.86493e-05 67.8571C17.752 67.8571 32.1429 82.248 32.1429 100L67.8572 100C67.8572 62.5235 37.4765 32.1429 3.70882e-05 32.1429L3.86493e-05 67.8571Z"/>    
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M100 67.8572C82.248 67.8572 67.8571 82.248 67.8571 100L32.1428 100C32.1428 62.5235 62.5235 32.1429 100 32.1429L100 67.8572Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M67.8571 2.5148e-06L67.8571 100L32.1428 100L32.1428 9.53674e-07L67.8571 2.5148e-06Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.86493e-05 67.8571C17.752 67.8571 32.1429 82.248 32.1429 100L67.8572 100C67.8572 62.5235 37.4765 32.1429 3.70882e-05 32.1429L3.86493e-05 67.8571Z"/>
     `,
     constraints: {
       top: true,
@@ -229,8 +228,8 @@ export const shapes = [
   },
   {
     id: 19,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M67.8574 99.8571C67.8574 82.1051 82.2482 67.7143 100 67.7143L100 32C62.5238 32 32.1431 62.3807 32.1431 99.8571L67.8574 99.8571Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M3.23994e-05 67.7146C17.752 67.7146 32.1429 82.1054 32.1429 99.8574L67.8572 99.8574C67.8572 62.381 37.4765 32.0003 3.39605e-05 32.0003L3.23994e-05 67.7146Z"/>
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M32.1429 100C32.1429 82.248 17.752 67.8571 0 67.8571V32.1428C37.4765 32.1428 67.8571 62.5235 67.8571 100H32.1429Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M99.9997 67.8571C82.2477 67.8571 67.8569 82.248 67.8569 100L32.1426 100C32.1426 62.5235 62.5233 32.1429 99.9997 32.1429L99.9997 67.8571Z"/>
     `,
     constraints: {
       top: false,
@@ -241,8 +240,8 @@ export const shapes = [
   },
   {
     id: 20,
-    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M100 67.7143L3.12224e-06 67.7143L0 32L100 32L100 67.7143Z"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.1429 99.8571C32.1429 82.1051 17.752 67.7143 0 67.7143L0 32C37.4765 32 67.8571 62.3807 67.8571 99.8571L32.1429 99.8571Z"/>
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M100 67.8571L3.12224e-06 67.8571L0 32.1428L100 32.1428L100 67.8571Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.1429 100C32.1429 82.248 17.752 67.8571 0 67.8571V32.1428C37.4765 32.1428 67.8571 62.5235 67.8571 100H32.1429Z"/>
     `,
     constraints: {
       top: false,
@@ -259,6 +258,18 @@ export const shapes = [
       right: false,
       bottom: false,
       left: false,
+    },
+  },
+  {
+    id: 22,
+    svg: `<path fill-rule="evenodd" clip-rule="evenodd" d="M100 32.1429L1.13523e-06 32.1429L0 67.8572L100 67.8572L100 32.1429Z"/>
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M32.1429 3.8266e-05C32.1429 17.752 17.752 32.1429 -1.98701e-06 32.1429L0 67.8572C37.4765 67.8572 67.8571 37.4765 67.8571 3.6279e-05L32.1429 3.8266e-05Z"/>
+    `,
+    constraints: {
+      top: true,
+      right: true,
+      bottom: false,
+      left: true,
     },
   },
 ];
