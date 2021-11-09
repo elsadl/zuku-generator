301bdf1efc972325b989d4b014675e422448e0de
Elsa Delmas <elsad@datagif.fr>
2021-11-09 14:56:50 +0100

    generate new pattern

--- src/App.svelte
+++ src/App.svelte
@@ -7,17 +7,18 @@
 
   let backgroundColor = "#FFFFFF";
 
+  let renew = false;
+
   $colors = allColors.filter((color) => color.hex != backgroundColor);
 
   const initGrid = (cols, rows) => {
-    let row = [];
-    for (let i = 0; i < cols; i++) {
-      row = [...row, null];
-    }
+    $gridComposition = [];
     for (let i = 0; i < rows; i++) {
-      $gridComposition = [...$gridComposition, [...row]];
+      $gridComposition[i] = [];
+      for (let j = 0; j < cols; j++) {
+        $gridComposition[i][j] = null;
+      }
     }
-    console.log($gridComposition);
   };
 
   initGrid($gridSize.cols, $gridSize.rows);
@@ -27,10 +28,18 @@
     $colors = allColors.filter((color) => color.hex != backgroundColor);
   };
 
+  const generateNewPattern = () => {
+    initGrid();
+    renew = true;
+    setTimeout(() => {
+      renew = false;
+    }, 1);
+  };
+
   const exportSvg = () => {
     svgExport.downloadSvg(
       document.querySelector("#grid-container svg"),
-      "zuku-pattern",
+      "zuku-pattern"
       // { width: $gridSize.cols * 100, height: $gridSize.rows * 100 }
     );
   };
@@ -43,6 +52,7 @@
   <div id="options">
     <!-- taille de la grille -->
     <div id="grid-size">
+      <h4>dimensions</h4>
       <div class="flex">
         <label for="">colonnes :</label>
         <input type="number" bind:value={$gridSize.cols} min="0" max="60" />
@@ -53,37 +63,63 @@
       </div>
     </div>
     <!-- couleur de fond -->
-    <ul id="bg-colors">
-      {#each allColors as color}
-        <li
-          on:click={() => changeBackgroundColor(color.hex)}
-          class={color.hex === backgroundColor ? "selected" : ""}
-        >
-          {color.name}
-        </li>
-      {/each}
-    </ul>
+    <div>
+      <h4>couleur de fond</h4>
+      <ul id="bg-colors">
+        {#each allColors as color}
+          <li
+            on:click={() => changeBackgroundColor(color.hex)}
+            class={color.hex === backgroundColor ? "selected" : ""}
+          >
+            {color.name}
+          </li>
+        {/each}
+      </ul>
+    </div>
     <!-- exportation -->
-    <p on:click={exportSvg}>Exporter</p>
+    <div>
+      <h4 class="button" on:click={generateNewPattern}>
+        générer un nouveau motif
+      </h4>
+    </div>
+    <div>
+      <h4 class="button" on:click={exportSvg}>exporter au format svg</h4>
+    </div>
   </div>
 
   <div id="grid-container">
     <svg width={$gridSize.cols * 100} height={$gridSize.rows * 100}>
-      <rect width={$gridSize.cols * 100} height={$gridSize.rows * 100} x=0 y=0 fill={backgroundColor}></rect>
-      {#each [...Array($gridSize.rows).keys()] as _, i}
-        {#each [...Array($gridSize.cols).keys()] as _, j}
-          <Shape row={i} col={j} />
+      <rect
+        width={$gridSize.cols * 100}
+        height={$gridSize.rows * 100}
+        x="0"
+        y="0"
+        fill={backgroundColor}
+      />
+      {#if !renew}
+        {#each [...Array($gridSize.rows).keys()] as _, i}
+          {#each [...Array($gridSize.cols).keys()] as _, j}
+            <Shape row={i} col={j} />
+          {/each}
         {/each}
-      {/each}
+      {/if}
     </svg>
   </div>
 </div>
 
 <style>
-  :global(body) {
-    margin: 0;
-    padding: 0;
-    color: black;
+  #options {
+    position: fixed;
+    z-index: 10;
+    background-color: white;
+    border: 1px solid;
+    padding: 1em;
+    top: 2em;
+    left: 2em;
+  }
+
+  #options > * + * {
+    padding-top: 1em;
   }
 
   #grid-container {
@@ -93,23 +129,9 @@
     height: 100vh;
     width: 100vw;
   }
-  
-/* #grid-container svg {
-    background-color: var(--bg-color);
-  } */
-
-  #grid-size {
-    position: absolute;
-    top: 2em;
-    left: 2em;
-  }
 
   #bg-colors {
-    position: absolute;
     display: flex;
-    list-style: none;
-    top: 2em;
-    right: 2em;
   }
 
   #bg-colors li {
@@ -124,12 +146,18 @@
     border-bottom: 1px solid;
   }
 
-  .flex {
-    display: flex;
-    align-items: baseline;
+  .button {
+    cursor: pointer;
+    border-bottom: 1px solid transparent;
+    display: inline-block;
   }
 
-  .flex label {
-    margin-right: 0.8em;
+  .button:hover {
+    border-bottom: 1px solid black;
+  }
+
+  .button::before {
+    content: "→";
+    padding-right: 0.3em;
   }
 </style>
